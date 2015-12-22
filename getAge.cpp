90ee981cc90a3d94effae8144cdb38958f3eafd8
Alfio Raymond <alfior73@users.noreply.github.com>
2015-12-21 19:55:00 -0500

    Create getAge.cpp

---
+++ getAge.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int age;
+	 
+	 cout << "Enter your age: " << endl;
+	 
+	 cin >> age;
+	 
+	 cout << endl << "You are " << age << " years old today" << endl;
+	 cout << endl << "You will be " << age + 10 << " years old in 10 years." << endl;
+	 
+	 return 0;
+}
