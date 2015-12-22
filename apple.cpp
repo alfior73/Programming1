14e56d3fd2c50cadebd5b8a8dfe66c2210fe997e
Alfio Raymond <alfior73@users.noreply.github.com>
2015-12-21 19:44:41 -0500

    Create apple.cpp

---
+++ apple.cpp
@@ -0,0 +1,13 @@
+void tree( int  apples);
+
+int  main ( )
+
+ {       for (int i=12; i > 1; i -= 5) 
+
+                 tree(i);
+
+          return (0);   }
+
+void tree( int  apples)
+
+{              cout <<  apples << " apples left! "; }
