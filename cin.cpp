3b68c128b85bf313e0d31bc90dc0872922422675
Alfio Raymond <alfior73@users.noreply.github.com>
2015-12-21 19:49:29 -0500

    Create cin.cpp

---
+++ cin.cpp
@@ -0,0 +1,27 @@
+// File Name: cin.cpp
+// Written by: Alfio Raymond
+// Description: Enter input
+// Challenges: none
+// Time Spent: 15 minutes
+// Given Input: (3 sets)         Expected Output: (3 corresponding sets)
+// --------------------          -------------------------
+// 1)
+// 2)
+// 3)								
+// Revision History
+// Date:        Revised By:     Action:
+// ------------------------------------------------------------------
+// mm/dd/yy    (your initials)  (what you did to the code)
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	int num1;
+	
+	cin >> num1;
+	cout << "\nNum1 is " << num1 << endl;
+	
+	return 0;
+}
