73af63a575ef4ace32848c9cc2992fe95998bd0c
Alfio Raymond <alfior73@users.noreply.github.com>
2016-01-11 22:34:18 -0500

    Update codeHeader.cpp

--- codeHeader.cpp
+++ codeHeader.cpp
@@ -1,17 +1,19 @@
-// Filename:
-// Written by:
-// Created on:
-// Description:
-// Challenges:
-// Copyright: Â©2015 Alfio Raymond
-// Time Spent:
-// Given Input: (3 sets)         Expected Output: (3 corresponding sets)
-// --------------------          -------------------------
-// 1)
-// 2)
-// 3)
-// Revision History
-// Date:        Revised By:     Action:
-// ------------------------------------------------------------------
-// mm/dd/yy    (your initials)  (what you did to the code)
-Help wanted
+/****************************************************************************** 
+    Filename:
+    Written by:
+    Created on:
+    Description:
+    Challenges:
+    Copyright: Â©2015 Alfio Raymond
+    Time Spent:
+    Given Input: (3 sets)         Expected Output: (3 corresponding sets)
+    --------------------          -------------------------
+   1)
+   2)
+   3)
+   Revision History
+   Date:        Revised By:     Action:
+   ------------------------------------------------------------------
+   mm/dd/yy    (your initials)  (what you did to the code)
+   
+********************************************************************************/
