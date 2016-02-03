1a27b2374c98b460ff593afe7d24922f075182d4
Alfio Raymond <alfior73@users.noreply.github.com>
2016-02-02 20:42:00 -0500

    Update variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -14,7 +14,16 @@ int main()
 {
 	cout << endl << endl;
 	
+	double num1, num2, num3=50.1, num4, num5, num6;
+	double players = 100.2;
 	
+	cout << num1 << endl; 
+	cout << num2 << endl;
+	cout << num3 << endl;
+	cout << num4 << endl;
+	cout << num5 << endl;
+	cout << num6 << endl;
+	cout << players << endl;
 	
 	return 0;
 }
