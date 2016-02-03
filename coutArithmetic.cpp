5eaf7c994d2904db12d0f361d0bdb0dd42963a8e
Alfio Raymond <alfior73@users.noreply.github.com>
2016-02-02 19:59:19 -0500

    Update coutArithmetic.cpp

--- coutArithmetic.cpp
+++ coutArithmetic.cpp
@@ -12,13 +12,18 @@ using namespace std;
 
 int main()
 {
-	cout << endl << endl << endl;
+	cout << endl << endl;
 	int amount = 1;
 	int m = 50;
 	int n = 10;
 	int p = 5;
 	int total = 0;
 	
+	cout << 10.0 + 15 / 2 + 4.3 << endl << endl;
+	cout << 3.0 * 4 / 6 + 6 << endl << endl;
+	cout << 20.0 - 2 / 6 + 3 << endl << endl;
+	cout << 10 + 17 / 3.0 + 4 << endl << endl;
+	cout << 10 + 17 % 3 + 4 << endl << endl;
 	
 	total = m / p + n - 10 * amount;
 	
