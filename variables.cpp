ed4a6cf80e8ace7be6a04ea7524e73747cdc3641
Alfio Raymond <alfior73@users.noreply.github.com>
2016-02-02 20:11:04 -0500

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,20 @@
+/***********************************
+
+Name: Alfio Raymond
+Date 2 February 2016
+
+filename: coutArithmetic.cpp
+
+************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << endl << endl;
+	
+	
+	
+	return 0;
+}
