bb012174dc07cd1607d35985bc8e0cd4a52fa52d
Alfio Raymond <alfior73@users.noreply.github.com>
2016-02-02 19:49:37 -0500

    Create coutArithmetic.cpp

---
+++ coutArithmetic.cpp
@@ -0,0 +1,26 @@
+/***********************************
+
+Name: Alfio Raymond
+Date 2 February 2016
+
+filename: coutArithmetic.cpp
+
+************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << endl << endl << endl;
+	int amount = 1;
+	int m = 50;
+	int n = 10;
+	int p = 5;
+	
+	
+	amount = m / p + n - 10 * amount;
+	
+	cout << amount;
+	return 0;
+}
