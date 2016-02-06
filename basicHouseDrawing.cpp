964821af3a316f1e548789db88fb633258d6364a
Alfio Raymond <alfior73@users.noreply.github.com>
2016-02-05 19:28:27 -0500

    Create basicHouseDrawing.cpp

---
+++ basicHouseDrawing.cpp
@@ -0,0 +1,28 @@
+/****************************************************************************** 
+    Filename: basicHouseDrawing.cpp
+    Written by: Alfio Raymond
+    Created on: 5 February 2016
+    Description: create a home of sticks
+    Challenges: none
+    Copyright: ©2016 Alfio Raymond
+    Time Spent: 10 minutes
+       
+********************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "    /\\   " << endl;
+	cout << "   /  \\  " << endl;
+	cout << "  /    \\ " << endl;
+	cout << "  ------  " << endl;
+	cout << "  |     | " << endl;
+	cout << "  |     | " << endl;
+	cout << "  |     | " << endl;
+	cout << "  ------- " << endl;
+	
+	return 0;	
+}
