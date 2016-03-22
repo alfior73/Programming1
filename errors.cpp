9c4a5868c4ebca80de5876e51f6bf4c33746a39c
Alfio Raymond <alfior73@users.noreply.github.com>
2016-03-22 19:59:32 -0400

    Create errors.cpp

---
+++ errors.cpp
@@ -0,0 +1,22 @@
+/****
+Test errors:
+	int a = 10;
+	int b = 5;
+	int c = 8;
+	int temp = 0;
+
+    if (a < c && b > temp)
+       a += c;
+    else
+       a = a - c;
+    cout << a << "  " << b << "  " << c << endl;
+    
+Test errors:
+    int number = 10;
+	
+	number--;
+	number += pow(number, 2);
+	number = -number;
+
+cout << number;
+****/
